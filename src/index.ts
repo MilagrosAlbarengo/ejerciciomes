b3864de5c0a242f41975c91d4ed78f3cf31606fa
MilagrosAlbarengo <92064009+MilagrosAlbarengo@users.noreply.github.com>
2021-10-30 20:23:45 -0300

    tareaparaentregarterminadameses

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,42 @@ let mes:number=document.getElementById("mes");
 let btn=document.getElementById("btn");
 btn.addEventListener("click",()=>{
   let mes2=Number(mes.value);
-let veintiochodias=2;
-let treintadias=4,6,9,11;
-let treintayunodias=1,3,5,7,8,10,12;
-  switch (true) {
-  case (mes2=== veintiochodias):
+
+  switch (mes2) {
+  case 1
+    console.log("tiene 31 días");
+    break;
+    case 2:
     console.log("tiene 28 días");
     break;
-  case (mes2=== treintadias):
+    case 3:
+    console.log("tiene 31 días");
+    break;
+    case 4:
+    console.log("tiene 30 días");
+    break;
+    case 5:
+    console.log("tiene 31 días");
+    break;
+    case 6:
+    console.log("tiene 30 días");
+    break;
+    case 7:
+    console.log("tiene 31 días");
+    break;
+    case 8:
+    console.log("tiene 31 días");
+    break;
+    case 9:
+    console.log("tiene 30 días");
+    break;
+    case 10:
+    console.log("tiene 31 días");
+    break;
+  case 11:
     console.log("tiene 30 dias");
     break;
-  case (mes2=== treintayunodias):
+  case 12:
     console.log("tiene 31 días");
     break;
   default:
